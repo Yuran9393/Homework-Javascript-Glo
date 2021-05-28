b3f84e332b205f78427052eeef5bb31c169affa4
Yura Luttsev <y.luttsev@gmail.com>
2021-05-28 11:20:00 +0600

    Completed lesson02

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 'use strict';
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 230000;
+let income = 'Работа в такси';
+let addExpenses = 'Коммуналка, автомобиль, детский сад, интернет'.toLowerCase();
+let deposit = true;
+let mission = 3000000;
+let period = 12;                            //Бюджет за 1 день
 
-alert('Hello World');
-console.log('Hello world');
+console.log( typeof(money), typeof(income), typeof(deposit) );
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' тенге');
+console.log(addExpenses.split(' , '));              //разбиваем строку на массив
+
+let budgetDay= money / 30;
+console.log(budgetDay.toFixed(0) + ' тенге');                 //количество символов после запятой
