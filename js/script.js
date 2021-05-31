865a30e5a8403e313f65744f1344231d154199b3
Yura Luttsev <y.luttsev@gmail.com>
2021-05-31 22:12:31 +0600

    Исправленное основное задание

--- js/script.js
+++ js/script.js
@@ -1,49 +1,34 @@
 'use strict';
-let money = 230000;
+let money = +prompt('Ваш месячный доход');
 let income = 'Работа в такси';
-let addExpenses = 'Коммуналка, автомобиль, детский сад, интернет'.toLowerCase();
-let deposit = true;
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую').toLowerCase();
+let deposit = confirm('Есть ли у вас депозит в банке?');
 let mission = 3000000;
-let period = 12;                         
-let budgetDay;
-let expenses1;
-let expenses2;
-let amount1;
-let amount2;
-let budgetMonth;
-let target;
-
-money=+prompt('Ваш месячный доход');
-addExpenses=prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-deposit=confirm('Есть ли у вас депозит в банке?');
-expenses1=prompt('Введите обязательную статью расходов?');
-amount1=+prompt('Во сколько это обойдется?');
-expenses2=prompt('Введите обязательную статью расходов?');
-amount2=+prompt('Во сколько это обойдется?');
-
-budgetMonth= money - ( amount1 + amount2 );
-target= mission / budgetMonth;
-budgetDay= budgetMonth / 30;
+let expenses1 = prompt('Введите обязательную статью расходов?');
+let amount1 = +prompt('Во сколько это обойдется?');
+let expenses2 = prompt('Введите обязательную статью расходов?');
+let amount2 = +prompt('Во сколько это обойдется?');
+let period = 12;         
+let budgetMonth = money - ( amount1 + amount2 );                
+let budgetDay = budgetMonth / 30;
+let target = mission / budgetMonth;
 
 // Рассчёт в тенге 1Р = 5,84тенге
 if (budgetDay >= 12000) {
     console.log('У вас высокий уровень дохода');
-}
-if (budgetDay > 6000 && budgetDay < 12000) {
+} else if (budgetDay > 6000 && budgetDay < 12000) {
     console.log('У вас средний уровень дохода');
-}
-if (budgetDay <= 6000 && budgetDay >= 0) {
+} else if (budgetDay <= 6000 && budgetDay >= 0) {
     console.log('К сожалению у вас уровень дохода ниже среднего');
-}
-if (budgetDay < 0){
+} else if (budgetDay < 0){
     console.log('Что то пошло не так');
 }
 
-console.log( typeof(money), typeof(income), typeof(deposit) );
+console.log(typeof money, typeof income, typeof deposit);
 console.log(addExpenses.length);
 console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' тенге');
-console.log(addExpenses.split(' , '));                          //разбиваем строку на массив
+console.log(addExpenses.split(' , '));                                          //разбиваем строку на массив
 console.log('Бюджет на месяц составляет ' + budgetMonth + ' тенге');
 console.log('Бюджет на один день составляет ' + Math.floor(budgetDay) + ' тенге');              
 console.log('Цель будет достигнута за ' + Math.ceil(target) + ' месяцев');
