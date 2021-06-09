9a700ce62006a40d2f392e24bd34369baee4218a
Yura Luttsev <y.luttsev@gmail.com>
2021-06-09 11:47:39 +0600

    ДЗ 9 урок

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
 'use strict';
+const btnStart = document.getElementById('#start');
+const btnIncomeAdd = document.getElementsByTagName('button');
+const btnExpensesAdd = document.getElementsByTagName('button');
+const depositCheck = document.querySelector('#deposit-check');
+const additionalIncomeItem = document.querySelectorAll('.additional_income-item');
+const budgetMonthValue = document.getElementsByClassName('.budget_month-value');
+const budgetDayValue = document.getElementsByClassName('.budget_day-value');
+const expensesMonthValue = document.getElementsByClassName('.expenses_month-value');
+const additionalIncomeValue = document.getElementsByClassName('.additional_income-value');
+const additionalExpensesValue = document.getElementsByClassName('.additional_expenses-value');
+const incomePeriodValue = document.getElementsByClassName('.income_period-value');
+const targetMonthValue = document.getElementsByClassName('.target_month-value');
+const salaryAmount = document.querySelector('.salary-amount');
+const incomeTitle = document.querySelector('.income-title');
+const incomeAmount = document.querySelector('.income-amount');
+const expensesAmount = document.querySelector('.expenses-amount');
+const additionalExpensesItem = document.querySelector('.additional_expenses-item');
+const targetAmount = document.querySelector('.target-amount');
+const periodSelect = document.querySelector('.period-select');
+
 let  money,
 isNumber = function(n) {
     return !isNaN(parseFloat(n) && isFinite(n));
